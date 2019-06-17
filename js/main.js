2d42d03493ebd67ff2f008199df326b20c3449fd
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-06-17 17:08:57 +0400

    Упростила код, удалила лишние функции

--- js/main.js
+++ js/main.js
@@ -1,125 +1,67 @@
 'use strict';
 
-// Функция генерирует числа от min до max и возвращает массив сгенерированных чисел
-var getNumbers = function (min, max, arr) {
-  var number = min;
-  for (var i = 0; i <= (max - min); i++) {
-    arr.push(number);
-    number = number + 1;
-  }
+var COMMENTS_AVATAR = [
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg',
+];
+var COMMENTS_MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var COMMENTS_NAME = ['Изольда', 'Марк', 'Луиза', 'Лев', 'Джана', 'Эрик'];
+var PHOTO_COUNT = 25;
 
-  return arr;
+
+var generatePhotoObject = function (index) {
+  return {
+    url: 'photos/' + index + '.jpg',
+    likes: getRandomNumber(15, 200),
+    comments: getCommentsArray()
+  };
 };
 
-var NUMBERS_PHOTO = [];
-
-getNumbers(1, 25, NUMBERS_PHOTO);
-
-// Функция перемешивает переданный массив
-var shuffleNumbers = function (arr) {
-  var j;
-  var temp;
-  for (var i = arr.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = arr[j];
-    arr[j] = arr[i];
-    arr[i] = temp;
-  }
-  return arr;
-};
-
-shuffleNumbers(NUMBERS_PHOTO);
-
-// Функция формирует массив адресов картинок из переданного массива чисел
-var getUrls = function (arrNumbers, arrUrls, startLine, endLine) {
-  for (var i = 0; i < arrNumbers.length; i++) {
-    var url = startLine + arrNumbers[i] + endLine;
-    arrUrls.push(url);
-  }
-  return arrUrls;
-};
-
-var URLS = [];
-
-getUrls(NUMBERS_PHOTO, URLS, 'photos/', '.jpg');
-
-// Функция генерирует произвольное число из переданного диапазона чисел
-var getRandomInteger = function (min, max) {
+var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-var NUMBERS_AVATAR = [];
-
-getNumbers(1, 6, NUMBERS_AVATAR);
-shuffleNumbers(NUMBERS_AVATAR);
-
-var AVATARS = [];
-
-getUrls(NUMBERS_AVATAR, AVATARS, 'img/avatar-', '.svg');
-
-var NAMES = ['Изольда', 'Марк', 'Луиза', 'Лев', 'Джана', 'Эрик'];
-
-// Функция создает массив объектов: список комментариев к фото
-var getComment = function (arrAvatars, arrMessages, arrNames, arrComments) {
-  var numbersComment = getRandomInteger(0, 4);
-  var stopPoint = numbersComment;
-  for (var i = 0; i <= stopPoint; i++) {
-    var comment = {};
-    comment.avatar = arrAvatars[getRandomInteger(0, arrAvatars.length - 1)];
-    comment.message = arrMessages[getRandomInteger(0, arrAvatars.length - 1)];
-    comment.name = arrNames[getRandomInteger(0, arrAvatars.length - 1)];
-    arrComments.push(comment);
+var getCommentsArray = function () {
+  var commentsCount = getRandomNumber(1, 10);
+  var commentsArray = [];
+  for (var i = 0; i < commentsCount; i++) {
+    commentsArray.push(generateCommentObject());
   }
-  return arrComments;
+  return commentsArray;
 };
 
-// getComment(AVATARS, MESSAGES, NAMES, COMMENTS);
-
-// console.log(COMMENTS);
-
-var COMMENTS = [];
-var ALL_COMMENTS = [];
-
-// Функция создает массив, который состоит из массивов объектов-списков комментариев
-var getPoolComments = function (arrAvatars, arrMessages, arrNames, arrComments, arrUrls, arrAllComments) {
-  for (var i = 0; i < arrUrls.length; i++) {
-    arrAllComments.push(getComment(arrAvatars, arrMessages, arrNames, arrComments));
-  }
-
-  return arrAllComments;
+var generateCommentObject = function () {
+  return {
+    avatar: getRandomItemFromArray(COMMENTS_AVATAR),
+    message: getRandomItemFromArray(COMMENTS_MESSAGE) + ' ' + getRandomItemFromArray(COMMENTS_MESSAGE),
+    name: getRandomItemFromArray(COMMENTS_NAME)
+  };
 };
 
-getPoolComments(AVATARS, MESSAGES, NAMES, COMMENTS, URLS, ALL_COMMENTS);
-
-// console.log(ALL_COMMENTS);
-
-var PICTURES = [];
-
-// Функция генерирует массив из JS-объектов (описание фото)
-var getPictures = function (arrUrls, arrAllComments, arrPictures) {
-  for (var i = 0; i < arrUrls.length; i++) {
-    var picture = {};
-    picture.url = arrUrls[i];
-    picture.likes = getRandomInteger(15, 200);
-    picture.comments = arrAllComments[i];
-    arrPictures.push(picture);
-  }
-  return arrPictures;
+var getRandomItemFromArray = function (arr) {
+  return arr[getRandomNumber(0, arr.length - 1)];
 };
 
-getPictures(URLS, ALL_COMMENTS, PICTURES);
-
-// console.log(PICTURES);
+var photosArray = [];
+for (var k = 0; k < PHOTO_COUNT; k++) {
+  photosArray.push(generatePhotoObject(k + 1));
+}
 
 var similarListElement = document.querySelector('.pictures');
 var similarPictureTemplate = document.querySelector('#picture')
 .content
 .querySelector('.picture');
-
-// console.log(similarListElement);
-// console.log(similarPictureTemplate);
 
 // Функция создает DOM-элемент на основе JS-объекта
 var renderPicture = function (picture) {
@@ -131,17 +73,15 @@ var renderPicture = function (picture) {
   return pictureElement;
 };
 
-// console.log(renderPicture(PICTURES[0]));
-
 // Функция заполняет блок DOM-элементами из массива
-var createFragment = function (pictures) {
+var fillBlockWithPictures = function (pictures) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < pictures.length; i++) {
     fragment.appendChild(renderPicture(pictures[i]));
   }
 
-  return similarListElement.appendChild(fragment);
+  similarListElement.appendChild(fragment);
 };
 
-createFragment(PICTURES);
+fillBlockWithPictures(photosArray);
