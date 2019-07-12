ba55b71d2d7bfd2a2b973507f1b2b1aeeed3277f
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-07-12 15:15:42 +0400

    Добавила функцию отправки данных формы

--- js/backend.js
+++ js/backend.js
@@ -1,33 +1,56 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/kekstagram/data';
+  var URL_DATA = 'https://js.dump.academy/kekstagram/data';
+  var URL_FORM = 'https://js.dump.academy/kekstagram';
   var TIMEOUT_VALUE = 10000;
 
-  window.load = function (onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
+  window.backend = {
+    load: function (onSuccess, onError) {
+      var xhr = new XMLHttpRequest();
 
-    xhr.responseType = 'json';
+      xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onSuccess(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
 
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения ' + xhr.status);
-    });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения ' + xhr.status);
+      });
 
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
 
-    xhr.timeout = TIMEOUT_VALUE;
+      xhr.timeout = TIMEOUT_VALUE;
 
-    xhr.open('GET', URL);
-    xhr.send();
+      xhr.open('GET', URL_DATA);
+      xhr.send();
+    },
+    upload: function (data, onSuccess, onError) {
+      var xhr = new XMLHttpRequest();
+
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onSuccess(xhr.response);
+        } else {
+          onError();
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError();
+      });
+
+      xhr.open('POST', URL_FORM);
+      xhr.send(data);
+    }
   };
 })();
